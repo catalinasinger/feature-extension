8ac6145283719c0a51a2e24f896db06925d46161
Shauni van der Horst <shaunivanderhorst@gmail.com>
2023-02-03 12:53:43 +0000

    Friday

--- client/src/Form.js
+++ client/src/Form.js
@@ -13,4 +13,6 @@ export default function IngredientForm() {
 )
 }
 
+
+
  
--- model/init_db.sql
+++ model/init_db.sql
@@ -19,10 +19,10 @@ ADD FOREIGN KEY (recipe_id) REFERENCES recipes(id);
 
 
 INSERT INTO recipes(name, image) VALUES ('Margarita', 'https://assets.bonappetit.com/photos/5b69f16006027f654a27cd19/1:1/w_2560%2Cc_limit/ba-margarita-1.jpg');
-INSERT INTO ingredients(text, recipe_id) VALUES ('Tequila 50ml', 1);
-INSERT INTO ingredients(text, recipe_id) VALUES ('Lime Juice 10ml', 1);
-INSERT INTO ingredients(text, recipe_id) VALUES ('Triple sec 10 ml', 1);
-INSERT INTO ingredients(text, recipe_id) VALUES ('Slice lime (garnish)', 1);
+INSERT INTO ingredients(text, recipe_id) VALUES ('Tequila', 1);
+INSERT INTO ingredients(text, recipe_id) VALUES ('Lime Juice', 1);
+INSERT INTO ingredients(text, recipe_id) VALUES ('Triple sec', 1);
+INSERT INTO ingredients(text, recipe_id) VALUES ('Slice lime', 1);
 
 INSERT INTO recipes(name, image) VALUES ('Cosmopolitan', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt8ZmPAyKfsb9htP3K7BOLcLiL6ajOvzFJZw&usqp=CAU');
 INSERT INTO ingredients(text, recipe_id) VALUES ('Vodka 30ml', 2);
