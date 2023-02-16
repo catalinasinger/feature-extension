b936aa73f20b15a4e81e0d8229b7a0c4be44a7c1
Catalina Singer <catalina.singer@gmail.com>
2023-02-16 16:08:26 +0100

    search bar working in both pages

--- client/src/Components/IngredientsCheckBox.js
+++ client/src/Components/IngredientsCheckBox.js
@@ -1,7 +1,50 @@
 import IngredientsData from "./IngredientsData"
 
 export default function IngredientsCheckBox() {
-    return (
-   <IngredientsData />
-    )
+   return ( 
+    <div class="block">
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+  <div class="mt-2">
+    <label class="inline-flex items-center">
+      <input type="checkbox" class="w-4 h-4"  />
+      <span class="ml-2">Small size checkbox</span>
+    </label>
+  </div>
+
+</div>
+  )
 }
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RecipesForm from "../Components/RecipesForm";
+import RecipesDisplay from "../Components/RecipesDisplay";
 import IngredientsCheckBox from "../Components/IngredientsCheckBox";
 
 export default function HomePage({ query }) {
-  /*const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState([]);
 
-  const handleAddRecipe = (newRecipe) => {
-    setRecipes((recipes) => [...recipes, newRecipe]);
+  const fetchRecipes = () => {
+    return fetch("http://localhost:5050/recipes")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data.recipes));
   };
-*/
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const filteredRecipes = recipes.filter((recipe) => {
+    return recipe.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  });
+
   return (
-   <div>
+    <>
+    {query ? 
+    <RecipesDisplay recipes={filteredRecipes} />
+    :
     <RecipesForm />
-   </div>
+    }
+   </>
   );
   }
   
