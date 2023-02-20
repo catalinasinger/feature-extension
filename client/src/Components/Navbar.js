528a925a18aeb9139cf8238a92d993326f702185
Catalina Singer <catalina.singer@gmail.com>
2023-02-20 21:13:48 +0100

    nre

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -5,9 +5,8 @@ function Navbar ({ onChange }){
 
     return (
         <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">MIXED</a>
-  </div>
+           <div className="flex-1">
+           </div>
   <div className="flex-none gap-2">
     <div className="form-control">
       <input 
